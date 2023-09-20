332a35507782b4335cde9fa1ff07daff007ba7f7
sou23-git <sooou23ohata@gmail.com>
2023-09-20 11:50:36 +0900

    ミスのとりあえず手直し、Constanceは未解決

--- app/src/main/java/app/pc_contest/tomato/HelpPageAdapter.kt
+++ app/src/main/java/app/pc_contest/tomato/HelpPageAdapter.kt
@@ -3,10 +3,11 @@ package app.pc_contest.tomato
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
 
 private const val NUM_PAGES = 2
 
-class HelpPageAdapter(fm: FragmentManager): HelpPageAdapter(fm) {
+class HelpPageAdapter(fm: FragmentManager): FragmentStatePagerAdapter(fm) {
 
     override fun getCount(): Int = NUM_PAGES
 
